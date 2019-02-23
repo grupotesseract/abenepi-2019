a2b947c773af4115617e696604d160b98705353a
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-02-23 18:41:31 -0300

    Aparentemente, funcionando envio da rota - testar no server

---
+++ app/Notifications/PagSeguroNotification.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Mail;
+use App\Models\TextosCubo;
+
+class PagSeguroNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Callback de notificação do PagSeguro
+     *
+     * @param [type] $information
+     * @return void
+     */
+    public static function pagseguro($information)
+    {
+        $response = PagSeguroNotification::setResponse($information);
+
+        $pagou = $response['status_transacao'] == 'paga';
+        \Log::info($response);        
+    }
+
+    public static function setResponse($information)
+    {
+
+        $status_transacao = strtolower($information->getStatus()->getName());
+        $sender = $information->getSender();
+        $email = $sender->getEmail();
+        $name = $sender->getName();
+
+        $response = [
+            'status_transacao' => $status_transacao,
+            'email' => $email,
+            'name' => $name,
+        ];
+
+        foreach ($information->getItems() as $item) {
+            $id_cubo = $item->getId();
+            $response['id_cubo'] = $id_cubo;
+        }
+
+        return $response;
+    }
+
+}
